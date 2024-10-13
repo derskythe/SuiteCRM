82c02546c4f017a5ffb6489c99ce032bb9343fb5
DerSkythe <skif@skif.ws>
2024-10-14 02:34:31 +0400

    feat: update data source configuration
    Renamed data source attributes and updated grants to new user 'app'.
    Changed default casing to 'lower/lower', and server version to '10.11.9'.
    Re-assigned schema and table associations to the new data source.

--- php_version.php
+++ php_version.php
@@ -4,7 +4,18 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 // The absolute minimum version on which to install SuiteCRM
+<<<<<<< HEAD:php_version.php
 define('SUITECRM_PHP_MIN_VERSION', '7.4.0');
 
 // The minimum recommended version on which to install SuiteCRM
 define('SUITECRM_PHP_REC_VERSION', '7.4.0');
+=======
+if (!defined('SUITECRM_PHP_MIN_VERSION')){
+    define('SUITECRM_PHP_MIN_VERSION', '8.1.0');
+}
+
+// The minimum recommended version on which to install SuiteCRM
+if (!defined('SUITECRM_PHP_REC_VERSION')){
+    define('SUITECRM_PHP_REC_VERSION', '8.2.0');
+}
+>>>>>>> 8cba9e2418 (feat: update data source configuration):public/legacy/php_version.php
